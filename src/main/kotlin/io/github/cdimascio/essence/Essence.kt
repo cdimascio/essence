31dc6343728cd0560c76bc5b289e2caaa8899d79
Clément P. (Cleymax) <clemperrin1@gmail.com>
2020-09-02 23:04:31 +0200

    ⚡️ Remove unnecessary function

--- src/main/kotlin/io/github/cdimascio/essence/Essence.kt
+++ src/main/kotlin/io/github/cdimascio/essence/Essence.kt
@@ -1,13 +1,7 @@
 package io.github.cdimascio.essence
 
 object Essence {
-    @JvmStatic
-    fun extract(html: String): EssenceResult {
-        return EssenceParser(html).parse()
-    }
 
     @JvmStatic
-    fun extract(html: String, language: Language? = null): EssenceResult {
-        return EssenceParser(html, language).parse()
-    }
+    fun extract(html: String, language: Language? = null): EssenceResult = EssenceParser(html, language).parse()
 }
